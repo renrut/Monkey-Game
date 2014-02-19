4c13d2c0682ac5169b592966aad9c3246bd1f59e
TurnerStrayhorn <winston.t.strayhorn@gmail.com>
2014-02-19 00:54:23 -0600

    Animations and Scripting
    I reimported the monkey with its arms separated and eyelids. I also
    animated the idle and fixed the problem with the monkeycontroller
    script not having the proper methods.
    TODO: Monkey Death/DeadIdle Animation, Animate Water so it still works
    with the collisions and y-increment, Sound effects.

---
+++ Assets/Scripts/monkeyController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class monkeyController : MonoBehaviour {
+
+
+
+
+	public void resetMonkey(){
+
+
+	}
+
+
+	public void killMonkey(){
+		//animation.Play("monkeyDie");
+		//animation.Play("deadIdle");
+
+		
+	}
+}
